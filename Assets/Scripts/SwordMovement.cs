2ec0bcaba332c8ff423a3f9b73d692a0b5ce555e
Cheesedoghotdog <jackhotcheese@gmail.com>
2025-06-24 17:33:05 -0500

    Sword Swinging Implemented
    Also added an Animator to the Main Square to get the console to shut up.

---
+++ Assets/Scripts/SwordMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordMovement : MonoBehaviour
+{
+    public Rigidbody2D swordBody;
+    public Camera cam;
+    Vector2 mousePos;
+    public float swingSpeed;
+    public float currentSwingSpeed;
+    public bool stabbing;
+    public float swordForce;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 lookDir = mousePos - swordBody.position;
+        float goalAngle = Mathf.Round(Mathf.Atan2(lookDir.x, -lookDir.y) * Mathf.Rad2Deg) + 180f;
+        if (swordBody.rotation > 360f) {
+            swordBody.rotation = swordBody.rotation - 360f;
+        } else if (swordBody.rotation < 0f) {
+            swordBody.rotation = swordBody.rotation + 360f;
+        }
+
+        if(goalAngle == Mathf.Round(swordBody.rotation) || goalAngle == Mathf.Round(swordBody.rotation + 360f) || goalAngle == Mathf.Round(swordBody.rotation + 180f)) {
+            swordForce = 0f;
+        } else if (goalAngle > swordBody.rotation && goalAngle < swordBody.rotation + 180f || goalAngle < swordBody.rotation - 180f && goalAngle > swordBody.rotation - 360f) {
+            swordBody.rotation = swordBody.rotation + currentSwingSpeed;
+            swordForce++;
+        } else {
+            swordBody.rotation = swordBody.rotation - currentSwingSpeed;
+            swordForce++;
+        }
+        Debug.Log(swordForce);
+
+        currentSwingSpeed = swingSpeed * (1 + (Mathf.Round(swordForce/100) / 4));
+
+    }
+}
