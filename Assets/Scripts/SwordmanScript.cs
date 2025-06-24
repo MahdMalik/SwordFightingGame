d8840b75c2520ab2914ee263673c5e62b04e59d9
MahdMalik <mahdmahdmalik@gmail.com>
2025-06-24 12:29:29 -0500

    alright, got basic opponent just existing for now, now I'd like to make them move towards the player and make it so they can take damage

--- Assets/Scripts/SwordmanScript.cs
+++ Assets/Scripts/SwordmanScript.cs
@@ -2,21 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
-public class PlayerMovement : MonoBehaviour
+public class SwordmanScript : MonoBehaviour
 {
+    // Start is called before the first frame update
     public float speed;
     private Animator animator;
-    // Start is called before the first frame update
+    private Random rng;
     void Start()
     {
         animator = GetComponent<Animator>();
+        rng = new Random();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float currentSpeed = Input.GetAxisRaw("Horizontal") * speed;
+        //just make their movement random for now
+        float currentSpeed = (float) (rng.NextDouble() * 2 - 1) * speed;
         transform.position = new Vector2(transform.position.x + currentSpeed * Time.deltaTime, transform.position.y);
         //this way if speed doesn't change, just keep it as is
         if (currentSpeed > 0)
