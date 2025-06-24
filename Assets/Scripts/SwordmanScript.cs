df657945b32e484676662c8fb28eac90845612e2
MahdMalik <mahdmahdmalik@gmail.com>
2025-06-24 13:38:44 -0500

    alright, now the opponent just follows you around

--- Assets/Scripts/SwordmanScript.cs
+++ Assets/Scripts/SwordmanScript.cs
@@ -8,6 +8,7 @@ public class SwordmanScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
+    public PlayerMovement player;
     private Animator animator;
     private Random rng;
     void Start()
@@ -20,7 +21,16 @@ public class SwordmanScript : MonoBehaviour
     void Update()
     {
         //just make their movement random for now
-        float currentSpeed = (float) (rng.NextDouble() * 2 - 1) * speed;
+        float currentSpeed = 0;
+        if (player.transform.position.x > transform.position.x)
+        {
+            //the random is gonna be between 0.5 and 1 through this. first it's between 0 and 1, then divide to get between 0 and .5, then add to get between .5 and 1.
+            currentSpeed = (float)(rng.NextDouble() / 2 + 0.5)  * speed;
+        }
+        else if (player.transform.position.x < transform.position.x)
+        {
+            currentSpeed = (float)(rng.NextDouble() / 2 + 0.5) * -1 * speed;
+        }
         transform.position = new Vector2(transform.position.x + currentSpeed * Time.deltaTime, transform.position.y);
         //this way if speed doesn't change, just keep it as is
         if (currentSpeed > 0)
