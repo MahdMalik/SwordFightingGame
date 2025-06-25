1f698243b53baa9ced9b4ce50058d79535d25cb0
Cheesedoghotdog <jackhotcheese@gmail.com>
2025-06-24 20:26:21 -0500

    Sword Turning Bug
    The sword won't turn for some reason. The transform is correctly rotating, but the stuff on screen isn't.

--- Assets/Scripts/SwordMovement.cs
+++ Assets/Scripts/SwordMovement.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class SwordMovement : MonoBehaviour
 {
-    public Rigidbody2D swordBody;
+    public Rigidbody2D swordControllerBody;
     public Rigidbody2D playerBody;
+    public Rigidbody2D swordBody;
     public Camera cam;
     Vector2 mousePos;
     public float swingSpeed;
@@ -15,29 +16,45 @@ public class SwordMovement : MonoBehaviour
 
     void Update()
     {
-        transform.position = playerBody.position;
+        transform.position = new Vector2 (playerBody.position.x, playerBody.position.y);
+
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 lookDir = mousePos - swordBody.position;
+        Vector2 lookDir = mousePos - swordControllerBody.position;
         float goalAngle = Mathf.Round(Mathf.Atan2(lookDir.x, -lookDir.y) * Mathf.Rad2Deg) + 180f;
-        if (swordBody.rotation > 360f) {
-            swordBody.rotation = swordBody.rotation - 360f;
-        } else if (swordBody.rotation < 0f) {
-            swordBody.rotation = swordBody.rotation + 360f;
+        if (swordControllerBody.rotation > 360f) {
+            swordControllerBody.rotation = swordControllerBody.rotation - 360f;
+        } else if (swordControllerBody.rotation < 0f) {
+            swordControllerBody.rotation = swordControllerBody.rotation + 360f;
         }
 
-        if(goalAngle == Mathf.Round(swordBody.rotation) || goalAngle == Mathf.Round(swordBody.rotation + 360f) || goalAngle == Mathf.Round(swordBody.rotation + 180f)) {
+        if(stabbing == false) {
+        if(goalAngle == Mathf.Round(swordControllerBody.rotation) || goalAngle == Mathf.Round(swordControllerBody.rotation + 360f) || goalAngle == Mathf.Round(swordControllerBody.rotation + 180f)) {
             swordForce = 0f;
-        } else if (goalAngle > swordBody.rotation && goalAngle < swordBody.rotation + 180f || goalAngle < swordBody.rotation - 180f && goalAngle > swordBody.rotation - 360f) {
-            swordBody.rotation = swordBody.rotation + currentSwingSpeed;
+        } else if (goalAngle > swordControllerBody.rotation && goalAngle < swordControllerBody.rotation + 180f || goalAngle < swordControllerBody.rotation - 180f && goalAngle > swordControllerBody.rotation - 360f) {
+            swordControllerBody.rotation = swordControllerBody.rotation + currentSwingSpeed;
             swordForce++;
+            Debug.Log(swordForce);
         } else {
-            swordBody.rotation = swordBody.rotation - currentSwingSpeed;
+            swordControllerBody.rotation = swordControllerBody.rotation - currentSwingSpeed;
             swordForce++;
+        }
         }
         //Debug.Log(swordForce);
 
         currentSwingSpeed = swingSpeed * (1 + (Mathf.Round(swordForce/100) / 4));
-
+        if(Input.GetMouseButton(0)) {
+            if (swordBody.position.y < 1.6) {
+            swordBody.position = new Vector2 (swordBody.position.x, swordBody.position.y + 0.01f);
+            }
+        } else {
+            if (swordBody.position.y > 1.0) {
+            swordBody.position = new Vector2 (swordBody.position.x, swordBody.position.y - 0.01f);
+            }
+        }
+        if (Mathf.Round(swordBody.position.y * 100) == 100.0) {
+            stabbing = false;
+        }
+        
 
     }
 }
