57ada25cc5579d63be6c165c88bef06bed4a3634
MahdMalik <mahdmahdmalik@gmail.com>
2025-06-25 03:28:37 -0500

    ok, so the issue is fixed now, nice.

--- Assets/Scripts/SwordMovement.cs
+++ Assets/Scripts/SwordMovement.cs
@@ -1,60 +1,110 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SwordMovement : MonoBehaviour
 {
-    public Rigidbody2D swordControllerBody;
-    public Rigidbody2D playerBody;
-    public Rigidbody2D swordBody;
+    public Rigidbody2D swordControllerBody; // Parent Rigidbody2D
+    public Rigidbody2D playerBody;          // Player Rigidbody2D
+    public Rigidbody2D swordBody;           // Child Rigidbody2D with its own physics
     public Camera cam;
-    Vector2 mousePos;
-    public float swingSpeed;
+    public float swingSpeed = 2f;
     public float currentSwingSpeed;
     public bool stabbing;
     public float swordForce;
 
+    // Local offset of swordBody relative to swordControllerBody, for stabbing animation
+    private Vector2 localOffset = new Vector2(0f, 1f);
+
     void Update()
     {
-        transform.position = new Vector2 (playerBody.position.x, playerBody.position.y);
+        // Move swordControllerBody to player position
+        swordControllerBody.position = playerBody.position;
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        // Get mouse position and compute goal rotation for parent
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        //this is like the vector where swordControllerBody is the origin, thus letting us easily get the angle
         Vector2 lookDir = mousePos - swordControllerBody.position;
-        float goalAngle = Mathf.Round(Mathf.Atan2(lookDir.x, -lookDir.y) * Mathf.Rad2Deg) + 180f;
-        if (swordControllerBody.rotation > 360f) {
-            swordControllerBody.rotation = swordControllerBody.rotation - 360f;
-        } else if (swordControllerBody.rotation < 0f) {
-            swordControllerBody.rotation = swordControllerBody.rotation + 360f;
+
+        //get the angle through quick arctangent, then get it in degrees. Note that it assumes the axis starts counter-clockwise to your right. To aline it to the top, subtract by 90.
+        float goalAngle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+
+        // Normalize rotation angle
+        float currentRotation = swordControllerBody.rotation;
+        //if it's negative, we want everything to be from 0 to 360, so do it that way
+        if (currentRotation < 0f) currentRotation += 360f;
+        //also, we want to make sure it doesn't go past 360
+        if (currentRotation >= 360f)
+            currentRotation %= 360f;
+
+        //change it so now it's correct
+        swordControllerBody.rotation = currentRotation;
+
+        //if not stabbing, do this
+        if (!stabbing)
+        {
+            //first, get distance left to travel
+            float diff = Mathf.DeltaAngle(currentRotation, goalAngle);
+
+            //if we can reach it now in the next frame, get to it
+            if (Mathf.Abs(diff) < currentSwingSpeed)
+            {
+                swordControllerBody.rotation = goalAngle;
+                swordForce = 0f;
+            }
+            //if not, grow closer to its location. If we have to go forwards to get closer, go forwards
+            else if (diff > 0)
+            {
+                swordControllerBody.rotation += currentSwingSpeed;
+                swordForce++;
+            }
+            //if have to go backwards to get closer, go backwards
+            else
+            {
+                swordControllerBody.rotation -= currentSwingSpeed;
+                swordForce++;
+            }
         }
 
-        if(stabbing == false) {
-        if(goalAngle == Mathf.Round(swordControllerBody.rotation) || goalAngle == Mathf.Round(swordControllerBody.rotation + 360f) || goalAngle == Mathf.Round(swordControllerBody.rotation + 180f)) {
-            swordForce = 0f;
-        } else if (goalAngle > swordControllerBody.rotation && goalAngle < swordControllerBody.rotation + 180f || goalAngle < swordControllerBody.rotation - 180f && goalAngle > swordControllerBody.rotation - 360f) {
-            swordControllerBody.rotation = swordControllerBody.rotation + currentSwingSpeed;
-            swordForce++;
-            Debug.Log(swordForce);
-        } else {
-            swordControllerBody.rotation = swordControllerBody.rotation - currentSwingSpeed;
-            swordForce++;
-        }
-        }
-        //Debug.Log(swordForce);
+        //calculate the new swing speed from the force.
+        currentSwingSpeed = swingSpeed * (1 + (swordForce / 400));
 
-        currentSwingSpeed = swingSpeed * (1 + (Mathf.Round(swordForce/100) / 4));
-        if(Input.GetMouseButton(0)) {
-            if (swordBody.position.y < 1.6) {
-            swordBody.position = new Vector2 (swordBody.position.x, swordBody.position.y + 0.01f);
+        // Adjust local stabbing offset on Y axis. First, see if pressed
+        if (Input.GetMouseButton(0))
+        {
+            //if it is, being held, extend to where it should go
+            if (localOffset.y < 1.6f)
+            {
+                stabbing = true;
+                localOffset.y += 0.01f;
+                swordForce = 300;
             }
-        } else {
-            if (swordBody.position.y > 1.0) {
-            swordBody.position = new Vector2 (swordBody.position.x, swordBody.position.y - 0.01f);
+
+        }
+        else
+        {
+            //otherwise, once button is let go, start retracting
+            if (localOffset.y > 1.0f)
+            {
+                localOffset.y -= 0.01f;
+                swordForce = 0;
             }
         }
-        if (Mathf.Round(swordBody.position.y * 100) == 100.0) {
+
+        //this way we can end the stabbing when it's retracted enough
+        if (Mathf.Approximately(localOffset.y, 1.0f))
             stabbing = false;
-        }
-        
+    }
 
+    void FixedUpdate()
+    {
+        // Calculate swordBody world position by rotating local offset by parent's rotation
+
+        //note that rotatedOffset is going to be in terms of x and y coordinates, how much to change it depending on the rotation
+        Vector2 rotatedOffset = Quaternion.Euler(0, 0, swordControllerBody.rotation) * localOffset;
+        Vector2 desiredPos = swordControllerBody.position + rotatedOffset;
+
+        // Move swordBody Rigidbody2D to desired position and rotation matching parent
+        swordBody.MovePosition(desiredPos);
+        swordBody.MoveRotation(swordControllerBody.rotation);
     }
 }
